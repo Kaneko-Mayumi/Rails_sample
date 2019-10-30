c45720ce753589b3ce7af8381917fc3843521d07
Kaneko <13tay.dia.s2@gmail.com>
2019-10-30 13:26:18 +0000

    commit20191030

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 15 }
   validates :email, presence: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
   validates :password, length: { minimum: 8, maximum: 32 }, format: {with: /\A[a-zA-Z0-9]+\z/}
-  validates :password_confirmation, presence: true, length: { minimum: 8, maximum: 32 }
+
 
   has_secure_password
 
-  
+  has_many :topics
 end
