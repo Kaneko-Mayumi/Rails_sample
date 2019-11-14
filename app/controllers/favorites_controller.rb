f0787d2448d380a725b6c0be2b19a9b50cf48fce
Kaneko <13tay.dia.s2@gmail.com>
2019-11-14 06:26:03 +0000

    commit20191114

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,14 +3,7 @@ class FavoritesController < ApplicationController
     @favorite_topics = current_user.favorite_topics
   end
   
-  def destroy
-    @favorite_topics =　Favorite.find_by(topic_id: params[:topic_id], user_id:current_user.id)
-    if @favorite_topics.destroy
-      redirect_to topics_path, success: 'お気に入りを解除しました'
-    else
-      redirect_to topics_path, danger: 'お気に入り解除に失敗しました'
-    end  
-  end
+
   
   def create
     favorite = Favorite.new
@@ -22,5 +15,16 @@ class FavoritesController < ApplicationController
     else
       redirect_to topics_path, danger: 'お気に入りに登録に失敗しました'
     end
+    
   end
+  
+  def destroy
+    favorite = Favorite.find_by(topic_id: params[:topic_id], user_id:current_user.id)
+    if favorite.destroy
+      redirect_to topics_path, success: 'お気に入りを解除しました'
+    else
+      redirect_to topics_path, danger: 'お気に入り解除に失敗しました'
+    end  
+  end
+  
 end
